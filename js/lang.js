1644dbba814921777e5cb133e9770bf2cc70b281
Melvyn Putra <alghifari041006@gmail.com>
2025-06-22 23:09:15 +0700

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -8,6 +8,9 @@ const translations = {
     chooseLang: "Pilih Bahasa",
     rumah: "Perumahan Candirenggo Asri Blok H-1 · Kabupaten Singosari · (62) 81330960798 ·",
     iam: " Saya adalah seorang semi-profesional jurusan Teknik Komputer dan Jaringan dengan keahlian di bidang keamanan Cyber dan juga berprestasi di bidang Teknik Jaringan, Administrasi Sistem, dan Manajemen Infrastruktur TI dengan Tingkat Komitmen Tinggi.",
+    FTTH: "FTTH (Fiber To The Home) (Serat Ke Rumah)",
+    FTTH2: "Magang Teknisi",
+    FTTH3: "Menginstal dan memelihara jaringan Fiber to the Home (FTTH), mengkonfigurasi perangkat ONU, menyambungkan kabel serat, dan menangani pemecahan masalah dasar, Memberikan dukungan teknis selama operasi lapangan.",
   },
   en: {
     about: "About",
@@ -18,6 +21,9 @@ const translations = {
     chooseLang: "Choose Language",
     rumah: "Candirenggo Asri Housing Block H-1 · Singosari Regency · (62) 81330960798 ·",
     iam: "I am a semi-professional majoring in Computer and Network Engineering with expertise in Cyber ​​security and also accomplished in Network Engineering, System Administration, and IT Infrastructure Management with High Commitment Level.",
+    FTTH: "FTTH (Fiber To The Home)",
+    FTTH2: "Technician Internship",
+    FTTH3: "Installed and maintained Fiber to the Home (FTTH) networks, Configured ONU devices, spliced fiber cables, and handled basic troubleshooting, Provided technical support during field operations.",
   }
 };
 
